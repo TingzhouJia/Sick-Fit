b28253bf04afff0aaa69b3f91826722dbd1fdaba
TingzhouJia <joe.tzjia@gmail.com>
2020-01-11 19:34:39 -0500

    add more datamodel

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,34 @@
 // let's go!
+const cookieParser=require('cookie-parser')
+const jwt=require('jsonwebtoken')
 require('dotenv').config({path:'variables.env'})
 const createServer=require('./createServer')
 const db=require('./db')
 const server= createServer();
+server.express.use(cookieParser())
+server.express.use((req,res,next)=>{
+    if(!req.cookies)return next()
+    
+    if(req.cookies.token){
+        const {userId}=jwt.verify(req.cookies.token,process.env.APP_SECRET)
+        
+        req.userId=userId
+    }
+   
+    next();
+   
+})
+
+// server.express.use(async (req, res, next) => {
+//     // if they aren't logged in, skip this
+//     if (!req.userId) return next();
+//     const user = await db.query.user(
+//       { where: { id: req.userId } },
+//       '{ id, permissions, email, name }'
+//     );
+//     req.user = user;
+//     next();
+//   });
 server.start({
     cors:{
         credentials:true,
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -8,7 +8,21 @@ const Query = {
     // }
     items:forwardTo('db'),//allow to use the same in prisma and yoga
     item: forwardTo('db'),
-    itemsConnection:forwardTo('db')
+    itemsConnection:forwardTo('db'),
+    me(parent, args, ctx, info) {
+      // check if there is a current user ID
+      console.log(ctx.request.userId)
+      if (!ctx.request.userId) {
+        return null;
+      }
+      return ctx.db.query.user(
+        {
+          where: { id: ctx.request.userId },
+        },
+        info
+      );
+    },
 };
 
 module.exports = Query;
+
